d2bd9c853d32c1fb5c1a857754632a57b45e51f9
Caroline Smith <caroline.smith87@gmail.com>
2017-12-11 01:07:14 +0000

    starting javadoc

--- src/cinema/CinemaMain.java
+++ src/cinema/CinemaMain.java
@@ -10,14 +10,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Contains the <code>main</code> method to launch the application. Defines navigation methods and sets up the logger. 
+ * @author carolinesmith, daianabassi
+ *@version 1.0
+ */
 public class CinemaMain extends Application {
 
-	// GET THE GLOBAL LOGGER
+	// GETS THE GLOBAL LOGGER
 	public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-	// DECLARE PUBLIC STAGE SO SCENES IN OTHER CLASSES CAN SET THE STAGE
+	// DECLARES PUBLIC STAGE SO SCENES IN OTHER CLASSES CAN SET THE STAGE
 	public static Stage thestage;
 
+	/**
+	 * Navigates to the next page. Loads the FXML passed to it and applies the page title specified. 
+	 * @param pathToFXML String relative path to FXML document to load
+	 * @param pageTitle String page title for the scene
+	 * @exception catches <code>javafx.fxml.LoadException</code> if unable to load FXML
+	 */
 	public void goToNextPage(String pathToFXML, String pageTitle) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource(pathToFXML));
@@ -32,18 +43,25 @@ public class CinemaMain extends Application {
 		}
 	}
 	
+	/**
+	 * Navigates to the log-in page. Loads the FXML passed to it and applies the page title specified. 
+	 * @param pathToFXML String relative path to FXML document to load
+	 * @param pageTitle String page title for the scene
+	 * @exception catches <code>javafx.fxml.LoadException</code> if unable to load FXML
+	 */
 	public void goToLoginPage(String pathToFXML, String pageTitle) {
 		
-		LoginController.userID = null;
+		//RESETS THE LOGGED IN USER AND CORRESPONDING ID AND USERNAME TO NULL
+		LoginController.userID = null; 
 		LoginController.loggedInUser = null;
 		LoginController.usersName = null;
 		
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource(pathToFXML));
+			Parent root = FXMLLoader.load(getClass().getResource(pathToFXML)); 
 
-			Scene scene = new Scene(root,1000, 800);
+			Scene scene = new Scene(root,1000, 800);  //SETS A NEW SCENE WITH THE LOADED FXML AND DEFINES THE WDITH/HEIGHT 
 			thestage.setTitle(pageTitle);
-			thestage.setScene(scene);
+			thestage.setScene(scene);   //SETS THE SCENE TO THE STAGE 
 			thestage.show();
 		} catch (Exception e) {
 			LOGGER.warning("Couldn't load that page");
@@ -54,20 +72,25 @@ public class CinemaMain extends Application {
 	
 
 	/**
-	 * Starting method for the program launched from 'main' Sets the stage to
-	 * scene1 - Login Screen 
-	 * @param primaryStage 
+	 * Method called from <code>cinema.CinemaMain.main</code>. Loads the first scene (LoginScreen.fxml).
+	 * @param primaryStage the primary stage of this JavaFX application
 	 */
 	@Override
 	public void start(Stage primaryStage) {
 
+		//SETS THE STATIC VARIABLE 'thestage' TO THE LOCAL 'primaryStage' VARIABLE
 		thestage = primaryStage;
 		
-		// LOADS 'LoginScreen.fxml' AND SETS THIS AS THE FIRST SCENE
+		// CALLS METHOD TO LOAD LOGIN SCREEN
 		goToLoginPage("shared_view/LoginScreen.fxml", "CinemaLogin");
 		
 	}
 
+	/**
+	 * The starting method for the application. Sets up the Logger from <code>cinema.CinemaLogger</code> and launches <code>cinema.CinemaMain.start</code>
+	 * @param args the array of arguments passed on launching the program
+	 * @exception catches IOException if an input or output exception occurred
+	 */
 	public static void main(String[] args) {
 
 		// SETS UP THE LOGGER FROM 'CinemaLogger.java'
@@ -77,7 +100,8 @@ public class CinemaMain extends Application {
 			e.printStackTrace();
 			throw new RuntimeException("Problems with creating the log files");
 		}
-
+	
+		//LAUNCHES THE JAVAFX APPLICATION
 		launch(args);
 	}
 }
--- src/cinema/customer_controllers/CustomerHomeController.java
+++ src/cinema/customer_controllers/CustomerHomeController.java
@@ -33,8 +33,16 @@ import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+/**
+ * Controller class for the Customer Home page. Sets up an image carousel and embedded YouTube videos at initialization.
+ * Defines functions to scroll backwards and forwards through image carousel and to navigate to different customer pages/log out.
+ * 
+ * @author carolinesmith, daianabassi
+ *
+ */
 public class CustomerHomeController implements Initializable {
 
+//DECLARES ALL THE FXML ELEMENTS USED BY THIS CONTROLLER//
 	@FXML
 	private ScrollPane homeAnchor;
 	@FXML
@@ -47,26 +55,33 @@ public class CustomerHomeController implements Initializable {
 	private VBox menuAnchor, vBox1, vBox2;
 	
 
-	Element root;
-	List list;
+	Element root;  //AN XML ROOT ELEMENT TO SET WHEN PARSING XML DOCUMENTS
+	List list;     //A LIST TO PASS THE XML NODES TO FROM XML PARSING
 
-	ArrayList<String> imageList = new ArrayList<String>();
+	ArrayList<String> imageList = new ArrayList<String>();  //AN ARRAYLIST TO PASS THE RELATIVE PATHS OF IMAGES TO FOR CAROUSEL
 
-	ImageView viewPic;
+	ImageView viewPic; //IMAGEVIEW TO SET THE CAROUSEL IMAGES TO
 
+	
+	/**
+	 * Called to initialize a controller after its root element has been completely processed.
+	 * Parses 'film.xml' file for film date information for setting up the carousel.
+	 * Loads embedded youtube videos to the home page.
+	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		
-
+		//URLS FOR EMBEDDED YOUTUBE VIDEOS 
 		String content_Url1 = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/19jGdktyGgk\" frameborder=\"0\" gesture=\"media\" allow=\"encrypted-media\" allowfullscreen></iframe>";
 		String content_Url2 = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/8O6eQBjDWDM\" frameborder=\"0\" gesture=\"media\" allow=\"encrypted-media\" allowfullscreen></iframe>";
+		//LOADS VIDEO CONTENT FROM YOUTUBE AND SETS TO VBOXs AT BOTTOM OF HOMEPAGE
 		loadVideo(content_Url1, vBox1);
 		loadVideo(content_Url2, vBox2);
 		
-
+		//PASSES 'film.xml' TO FILE
 		File xmlFile = new File("film.xml");
 
+		//IF 'film.xml' EXSISTS PARSES IT BY CALLING 'readsXML' FROM 'cinema.XML.ReadXMLFile'
 		if (xmlFile.exists()) {
 			try {
 				ReadXMLFile read = new ReadXMLFile("film.xml");
@@ -75,73 +90,78 @@ public class CustomerHomeController implements Initializable {
 				CinemaMain.LOGGER.warning("Couldn't parse film.XML");
 			}
 
-			// PARSES XML: ITERATES THROUGH THE 'FILM' LIST, GETS
-			// ELEMENT/ATTRIBUTES
-			// AND PASSES IT TO FILM VARIABLES
+			// GETS LIST OF CHILD NODES OF THE ROOT ELEMENT IN 'film.xml'
 			list = root.getChildren("film");
 
+			//ITERATES THROUGH THE LIST OF XML NODES TO EXRTACT FILM DATE/TIME INFORMATION 
+			//THIS WILL BE USED TO ENSURE IMAGES ARE NOT SET TO THE CAROUSEL FOR FILMS IN THE PAST
 			for (int i = 0; i < list.size(); i++) {
 
 				Element node = (Element) list.get(i);
 				String filmDateTimes = node.getChildText("dateTimes");
 
-				List<String> allDates = new ArrayList<String>();
+				List<String> allDates = new ArrayList<String>();  //ARRAYLIST TO PASS JUST THE FILM DATES TO 
 
-				for (String element : filmDateTimes.split(",")) {
-					String date = element.substring(0, 8);
+				for (String element : filmDateTimes.split(",")) {  
+					String date = element.substring(0, 8);    //EXTRACTS JUST THE DATE (NOT THE TIME)
 					allDates.add(date);
 				}
-				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yy");
+				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yy");  //CREATES INSTANCE OF 'SimpleDateFormat' TO PARSE STRING TO DATES
 				Date today = new Date();
-				Iterator<String> it = allDates.iterator();
-				while (it.hasNext()) {
+				Iterator<String> it = allDates.iterator();    //SETS UP AN ITERATOR TO ITERATE THROUGH 'allDates'
+				
+				while (it.hasNext()) {   
 					String element = it.next();
 
 					Date date = null;
 					try {
-						date = sdf.parse(element);
-					} catch (ParseException e) {
+						date = sdf.parse(element);     //PARSES EACH STRING IN allDates TO A DATE
+					} catch (ParseException e) {       //CATCHES PARSE EXCEPTION
 						e.printStackTrace();
 					}
-					if (today.compareTo(date) > 0)
+					if (today.compareTo(date) > 0)     //IF THE DATE IS BEFORE TODAY'S DATE REMOVES THE DATE FROM THE LIST
 						it.remove();
 				}
 
-				if (allDates.size() > 0)
-					imageList.add(node.getChildText("image"));
+				if (allDates.size() > 0)        	   //IF THE LIST OF DATE FOR THIS NODE (THIS FILM) IS >0 ADDS IT TO THE LIST OF IMAGES FOR CAROUSEL
+					imageList.add(node.getChildText("image"));      //i.e. IF THE FILM HAS AT LEAST ONE DATE IN THE FUTURE - DISPLAY AN IMAGE FOR IT
 			}
 		}
 
-		if (imageList.size() > 0) {
-			Image filmPic = new Image(imageList.get(0), 270, 400, false, false);
+		if (imageList.size() > 0) {                     // IF THERE ARE IMAGES TO DISPLAY (FILMS IN THE FUTURE) 
+			Image filmPic = new Image(imageList.get(0), 270, 400, false, false);   //ADD THE FIRST IMAGE TO THE CAROUSEL
 			viewPic = new ImageView(filmPic);
 			viewPic.setId("0");
 
-			carouselAnchor.getChildren().add(viewPic);
-			viewPic.fitWidthProperty().bind(carouselAnchor.widthProperty());
+			carouselAnchor.getChildren().add(viewPic);	//SETS THE IMAGEVIEW TO A PANE
+			viewPic.fitWidthProperty().bind(carouselAnchor.widthProperty());    //BINDS THE IMAGEVIEW SIZE TO THE PANE
 			viewPic.fitHeightProperty().bind(carouselAnchor.heightProperty());
 			viewPic.setPreserveRatio(false);
 		}
 
 	}
 
+	/**
+	 * Iterates forwards through the list of images for films with showings in the future. Gets the next image in the list
+	 * and replaces the current carousel image with it. 
+	 */
 	@FXML
 	public void scrollsForward() {
 
 		if (imageList.size() > 0) {
 
-			int a = Integer.parseInt(viewPic.getId());
+			int a = Integer.parseInt(viewPic.getId()); //GETS THE ID OF THE CURRENT IMAGE DISPLAYED AS INTEGER
 
-			if (a != imageList.size() - 1) {
-
-				ListIterator<String> it = imageList.listIterator(a);
+			if (a != imageList.size() - 1) {      	                   // IF ID DOESN'T EQUAL THE SIZE OF THE IMAGE LIST
+																	   
+				ListIterator<String> it = imageList.listIterator(a);   //ITERATES FORWARDS THROUGH IMAGE LIST
 
 				if (it.hasNext()) {
-					Image filmPic = new Image(imageList.get(a + 1), 200, 200, false, false);
-					viewPic = new ImageView(filmPic);
-					viewPic.setId(Integer.toString(a + 1));
+					Image filmPic = new Image(imageList.get(a + 1), 200, 200, false, false);   //GETS THE NEXT IMAGE IN THE LIST
+					viewPic = new ImageView(filmPic);											//SETS IT TO AN IMAGEVIEW
+					viewPic.setId(Integer.toString(a + 1));										//SETS IT'S ID TO ONE LARGER THAN CURRENT IMAGE
 
-					carouselAnchor.getChildren().add(viewPic);
+					carouselAnchor.getChildren().add(viewPic);									//DISPLAYS IT IN THE CAROUSEL (REPLACES CURRENT IMAGE)
 					viewPic.fitWidthProperty().bind(carouselAnchor.widthProperty());
 					viewPic.fitHeightProperty().bind(carouselAnchor.heightProperty());
 					viewPic.setPreserveRatio(false);
@@ -150,7 +170,11 @@ public class CustomerHomeController implements Initializable {
 		}
 
 	}
-
+	
+	/**
+	 * Iterates backwards through the list of images for films with showings in the future. Gets the next image in the list
+	 * and replaces the current carousel image with it. 
+	 */
 	@FXML
 	public void scrollsBack() {
 
@@ -158,16 +182,16 @@ public class CustomerHomeController implements Initializable {
 
 			int b = Integer.parseInt(viewPic.getId());
 
-			if (b != 0) {
-
+			if (b != 0) {													//STARTS ITERATING THROUGH LIST IF THE CURRENT IMAGE ISN'T THE FIRST
+																			//IN THE LIST (i.e. THE FIRST IMAGE DISPLAYED ON INITIALISATION)
 				ListIterator<String> itBack = imageList.listIterator(b);
 
-				if (itBack.hasPrevious()) {
-					Image filmPic = new Image(imageList.get(b - 1), 200, 200, false, false);
+				if (itBack.hasPrevious()) {									//ITERATES BACKWARDS THROUGH IMAGE LIST
+					Image filmPic = new Image(imageList.get(b - 1), 200, 200, false, false);   //GETS THE PREVIOUS IMAGE
 					viewPic = new ImageView(filmPic);
 					viewPic.setId(Integer.toString(b - 1));
 
-					carouselAnchor.getChildren().add(viewPic);
+					carouselAnchor.getChildren().add(viewPic);				//SETS IT TO THE CAROUSEL 
 					viewPic.fitWidthProperty().bind(carouselAnchor.widthProperty());
 					viewPic.fitHeightProperty().bind(carouselAnchor.heightProperty());
 					viewPic.setPreserveRatio(false);
@@ -177,7 +201,11 @@ public class CustomerHomeController implements Initializable {
 
 	}
 
-	
+/**
+ * Loads the embedded YouTube video to a WebView and adds this WebView a VBox.
+ * @param content String URL of the embedded YouTube video.
+ * @param vbox VBox to set the video to.
+ */
 	public void loadVideo(String content, VBox vbox) {
 
 		WebView webView = new WebView();
@@ -187,31 +215,47 @@ public class CustomerHomeController implements Initializable {
 		vbox.setPrefSize(560, 350);
 	}
 	
-	
-	
 
-	//////// NAVIGATION FUNCTIONS///////
+//////// NAVIGATION FUNCTIONS///////
+
+	/**
+	 * Loads the customer what's on page when 'book a film' button clicked or 'what's on' menu item selected.
+	 * Calls <code>goToNextPage</code> function from <code>cinema.CinemaMain</code>
+	 * @param event the button/menu item click event
+	 */
 	@FXML
 	private void loadfilms(ActionEvent event) {
-		// TAKES YOU TO 'WhatsOn' PAGE WHEN 'ALL FILM' BUTTON OR 'WHATS ON' MENU
-		// ITEM PRESSED
 		CinemaMain main = new CinemaMain();
 		main.goToNextPage("customer_view/WhatsOnCustomer.fxml", "What's On");
 	}
-
+	
+	/**
+	 * Takes use back to login page when log out menu item selected.
+	 * Calls <code>goToLoginPage</code> function from <code>cinema.CinemaMain</code>
+	 * @param event the menu item click event
+	 */
 	@FXML
 	private void logsOut(ActionEvent event) {
-		// TAKES YOU TO BACK TO LOGIN SCREEN
 		CinemaMain main = new CinemaMain();
 		main.goToLoginPage("shared_view/LoginScreen.fxml", "Cinema Login");
 	}
 
+	/**
+	 * Loads the customer account page when account menu item selected.
+	 * Calls <code>goToNextPage</code> function from <code>cinema.CinemaMain</code>
+	 * @param event the menu item click event
+	 */
 	@FXML
 	private void goToMyAccount() {
 		CinemaMain main = new CinemaMain();
 		main.goToNextPage("customer_view/CustomerAccount.fxml", "My Account");
 	}
 	
+	/**
+	 * Loads the customer questionnaire page when 'win a free ticket' button clicked.
+	 * Calls <code>goToNextPage</code> function from <code>cinema.CinemaMain</code>
+	 * @param event the button click event
+	 */
 	@FXML 
 	public void goesToQuestionnaire() {
 		CinemaMain main = new CinemaMain();
