6c78fb79fce4d2af53677a3f4cff0a053d860d23
db-assi <daiana.bassi.17@ucl.ac.uk>
2017-12-12 15:11:50 +0000

    small changes to style

--- src/cinema/shared_controllers/BookingController.java
+++ src/cinema/shared_controllers/BookingController.java
@@ -24,6 +24,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class BookingController extends WhatsOnEmpController implements Initializable {
 
